2a2b0e3dcc8648f1319d1deec58f5b6bff70270b
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-02-03 18:01:10 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,8 +5,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Clients, Pets, Services, Contracts, Messages, Images
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,26 +23,88 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/clients', methods=['GET'])
+def get_clients():
+    clients = Clients.query.all()
+    results = [client.serialize() for client in clients]
+    response_body = {'message': 'OK',
+                     'total_records': len(results),
+                     'results': results}
+    return jsonify(response_body), 200
+
+
+@app.route('/clients/<int:client_id>', methods=['GET'])
+def get_client(client_id):
+    client = Clients.query.get(client_id)
+    result = client.serialize()
+    response_body = {'message': 'OK',
+                     'result': result}
+    return jsonify(response_body), 200
+
+
+@app.route('/clients', methods=['POST'])
+def register_client():
+    request_body = request.get_json()
+    client = Clients(roles = request_body['roles'],
+                     name = request_body['name'],
+                     surname = request_body['surname'],
+                     email = request_body['email'],
+                     password = request_body['password'],
+                     avatar = request_body['avatar'],
+                     description = request_body['description'],
+                     city = request_body['city'])
+    db.session.add(client)
+    db.session.commit()
+    return jsonify(request_body), 200
+
+
+@app.route('/clients/<int:client_id>', methods=['DELETE'])
+def delete_client(client_id):
+    client = Clients.query.get(client_id)
+    db.session.delete(client)
+    db.session.commit()
+    return jsonify('OK'), 200
+
+
+@app.route('/clients/<int:client_id>', methods=['PUT'])
+def update_client(client_id):
+    client = Clients.query.get(client_id)
+    if client is None:
+        return 'Not found', 404
+
+    client.roles = request.json.get('roles', client.roles)
+    client.name = request.json.get('name', client.name)
+    client.surname = request.json.get('surname', client.surname)
+    client.email = request.json.get('email', client.email)
+    client.password = request.json.get('password', client.password)
+    client.avatar = request.json.get('avatar', client.avatar)
+    client.description = request.json.get('description', client.description)
+    client.city = request.json.get('city', client.city)
+    db.session.commit()
+
+    response_body = {'roles': client.roles,
+                     'name': client.name,
+                     'surname': client.surname,
+                     'email': client.email,
+                     'password': client.password,
+                     'avatar': client.avatar,
+                     'description': client.description,
+                     'city': client.city}
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,86 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Clients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    roles = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    avatar = db.Column(db.String)
+    description = db.Column(db.String)
+    city = db.Column(db.String)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Clients %r>' % self.id
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {'id': self.id,
+                'name': self.name,
+                'surname': self.surname,
+                'avatar': self.avatar,
+                'city': self.city,
+                'description': self.description}
+
+
+class Pets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    description = db.Column(db.String)
+    owner_id = db.Column(db.Integer, db.ForeignKey(
+        'clients.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Pets %r>' % self.id
+
+
+class Services(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    price = db.Column(db.Integer)
+    description = db.Column(db.String)
+    client_id = db.Column(db.Integer, db.ForeignKey(
+        'clients.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Services %r>' % self.id
+
+
+class Contracts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pet_id = db.Column(db.Integer, db.ForeignKey('pets.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey(
+        'services.id'), nullable=False)
+    date = db.Column(db.String)
+    price = db.Column(db.Integer)
+    assessment = db.Column(db.Integer)
+    comments = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Contracts %r>' % self.id
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transmitter_id = db.Column(
+        db.Integer, db.ForeignKey('clients.id'), nullable=False)
+    receiver_id = db.Column(
+        db.Integer, db.ForeignKey('clients.id'), nullable=False)
+    date = db.Column(db.String)
+    content = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Messages %r>' % self.id
+
+
+class Images(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey(
+        'clients.id'), nullable=False)
+    url = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Images %r>' % self.id
